0434f3c18cb6d8cd9b6885b3ea2914b33dcc33a0
werto87 <huhulasmichinruhe@web.de>
2023-11-18 16:24:32 +0900

    print compiler, build type and lib

--- main.cxx
+++ main.cxx
@@ -1,8 +1,29 @@
 #include "src/hello.hxx"
 #include <iostream>
+#include <boost/predef.h>
 int
 main ()
 {
-  std::cout << helloWorld () << std::endl;
+
+#if DEBUG
+    std::cout << "DEBUG" << std::endl;
+#else
+    std::cout << "RELEASE" << std::endl;
+#endif
+#if BOOST_COMP_GNUC
+    std::cout << "BOOST_COMP_GNUC" << std::endl;
+#endif
+#if BOOST_COMP_CLANG
+    std::cout << "BOOST_COMP_CLANG" << std::endl;
+#endif
+#if BOOST_LIB_C_GNU
+    std::cout << "BOOST_LIB_C_GNU" << std::endl;
+#endif
+#if BOOST_LIB_STD_CXX
+    std::cout << "BOOST_LIB_STD_CXX" << std::endl;
+#endif
+#if BOOST_LIB_STD_GNU
+    std::cout << "BOOST_LIB_STD_GNU" << std::endl;
+#endif
   return 0;
 }
