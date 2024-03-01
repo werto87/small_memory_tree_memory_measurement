53f8e894a3c95ee54e9c4338b2420eca86dc296e
walde <w-schneider1987@web.de>
2024-03-01 21:12:15 +0900

    generate compile_command

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,16 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain
 
 
 class Project(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators =  "CMakeDeps"
+
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False #workaround because this leads to useless options in cmake-tools configure
+        tc.generate()
 
     def configure(self):
         # We can control the options of our dependencies based on current options
@@ -13,4 +20,4 @@ class Project(ConanFile):
 
     def requirements(self):
         self.requires("catch2/2.13.7")
-        self.requires("st_tree/1.2.2")
+        self.requires("small_memory_tree/4.1.1")
--- main.cxx
+++ main.cxx
@@ -1,11 +1,15 @@
-#include <st_tree.h>
+#include <cstdint>
+#include <small_memory_tree/smallMemoryTreeLotsOfChildren.hxx>
 
 using namespace st_tree;
 
 int
 main ()
 {
-  tree<int> t;
-  t.insert(1);
+  tree<uint64_t> t{};
+  t.insert (1);
+  for (auto i = uint64_t{}; i < 1000; ++i)
+    {
+    }
   return 0;
 }
